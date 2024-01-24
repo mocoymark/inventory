c32153cc6efd516bb45ecb71abe949b582d55c6f
Mark Mocoy <mmocoy61@gmail.com>
2024-01-25 02:17:18 +0800

    delteBtn

--- admin/api/action.php
+++ admin/api/action.php
@@ -5,12 +5,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_POST['id'];
     $action = $_POST['action'];
 
-    // Update the user status based on the action
-    $status = ($action === 'approve') ? 'approved' : 'rejected';
-    $sql = "UPDATE `usertbl` SET `status` = ? WHERE `id` = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('si', $status, $userId);
-    $stmt->execute();
-    $stmt->close();
+    // $status = ($action === 'approve') ? 'approved' : 'rejected';
+     
+    if ($action === 'approve') {
+    $status = 'approved';
+     }elseif($action == 'delete'){
+       $sql = "DELETE FROM `usertbl` WHERE `id` = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('i', $userId);
+        $stmt->execute();
+        $stmt->close();
+     } else {
+    $status = 'rejected';
+      }
+    $conn->query("UPDATE `usertbl` SET `status` = '$status' WHERE `id` = $userId");
 }
 ?>
